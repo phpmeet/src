7b3198b7076ad21f2a8b0e680205ca6ea52837de
phpmeet@163.com <phpmeet@163.com>
2021-03-09 12:28:15 +0800

    upload

--- app/home/controller/IndexController.php
+++ app/home/controller/IndexController.php
@@ -7,8 +7,7 @@ use core\Template;
 class IndexController extends Controller{
     public function index(){
         $html = "<body><div style='font-size:20px;font-weight:500;text-align: center'>phpmeet</div></body>";
-        echo $html;
-        die;
+        echo $html;die;
     }
 
 }
