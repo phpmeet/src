20b683b0a06e6903d229d4b57f129823fefaf1c5
phpmeet@163.com <phpmeet@163.com>
2021-03-09 12:29:30 +0800

    upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-git commit -a -m "upload"
-git push origin
+/usr/bin/git commit -a -m "upload"
+/usr/bin/git push origin
 
 curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/bitbucket?username=phpmeet&apiToken=yXrVOTAHoCLSK2OEadGR' -d'{"repository":{"url":"https://gitee.com/phpmeet/src.git"}}'
